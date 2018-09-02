0252cc002010454617874a29b698d22a652d17a8
Ravindra <ravindra.spatil@gmail.com>
2018-09-02 22:29:37 +0530

    Upload product image URL change

--- api/routes/products.js
+++ api/routes/products.js
@@ -8,9 +8,7 @@ const ProductsController = require('../controllers/products');
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
-        // cb(null, './uploads/');
-        cb(null, 'https://rocky-thicket-37577.herokuapp.com/uploads/');
-
+        cb(null, './uploads/');
     },
     filename: function(req, file, cb) {
         cb(null, new Date().toISOString().replace(/:/g, '-') + file.originalname);
